896672baf9072967090e6a5b2994f62be024c283
HrenovSystemshik <blackjets@mail.ru>
2021-08-07 20:13:42 +0300

    add user check

--- src/test/java/ru/hh/spb/YandexPageTest.java
+++ src/test/java/ru/hh/spb/YandexPageTest.java
@@ -7,11 +7,12 @@ import org.openqa.selenium.WebElement;
 public class YandexPageTest extends SetupClass {
 
     private static String defaultTestPage;
+    private static String userXPath = "//*[@class ='button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited']";
 
-    boolean isClickable (WebElement element){
-        try{
+    boolean isClickable(WebElement element) {
+        try {
             element.click();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return false;
         }
         return true;
@@ -21,38 +22,69 @@ public class YandexPageTest extends SetupClass {
     public static void beforeClass() throws Exception {
         defaultTestPage = "https://yandex.ru/";
     }
+
     @Before
     @After
     public void defaultConditions() {
         driver.get(defaultTestPage);
     }
 
+    /**
+     * Поиск кнопки по тексту.
+     * без /.. просто находит текст.
+     * /.. берем родителя
+     */
     @Test
     public void getSearchByText() {
         WebElement searchBtn = driver.findElement(
                 new By.ByXPath("//*[text() = 'Найти']/.."));
         Assert.assertTrue(isClickable(searchBtn));
     }
+
+    /**
+     * Поиск кнопки по классу.
+     */
     @Test
-    public void getSearchByXPath(){
+    public void getSearchByXPath() {
         WebElement searchBtn = driver.findElement(
                 new By.ByXPath("//*[@class ='button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited']")
         );
         Assert.assertTrue(isClickable(searchBtn));
     }
+
+    /**
+     * Из найденной кнопки берем текст. Указанием наследника [1]
+     */
     @Test
-    public void checkThatBtnIsSearch1(){
+    public void checkThatBtnIsSearch1() {
         WebElement searchBtn = driver.findElement(
                 new By.ByXPath("//*[@class ='button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited'][1]")
         );
         Assert.assertEquals("Найти", searchBtn.getText());
     }
+
+    /**
+     * Из найденной кнопки берем дочерний элемент, указав точку перед родителем.
+     */
     @Test
-    public void checkThatBtnIsSearch2(){
+    public void checkThatBtnIsSearch2() {
         WebElement searchBtn = driver.findElement(
                 new By.ByXPath(".//*[@class ='button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited']")
         );
         Assert.assertEquals("Найти", searchBtn.getText());
+    }
+
+    /**
+     * Проверяем пользовательский XPath путем поиска фразы I Love Luxoft
+     */
+    @Test
+    public void checkUserSearch() {
+        WebElement searchBtn = driver.findElement(
+                new By.ByXPath(userXPath)
+        );
+        WebElement searchInput = driver.findElement(new By.ByXPath("//input[@class = 'input__control input__input mini-suggest__input']"));
+        searchInput.sendKeys("I love Luxoft");
+        Assert.assertTrue(isClickable(searchBtn));
     }
 
 
